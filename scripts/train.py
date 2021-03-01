154f7305f109c412ba374f4bf11d1a30fd8105b5
Roshan Khatale <rvkhatale@gmail.com>
2021-03-01 14:00:00 +0530

    Add training script

--- scripts/train.py
+++ scripts/train.py
@@ -2,12 +2,11 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import joblib
 
-data = pd.read_csv("data/students.csv")
-X = data[['Hours']]
-y = data['Scores']
+df = pd.read_csv("data/students.csv")
+X = df[['Hours']]
+y = df['Scores']
 
 model = LinearRegression()
 model.fit(X, y)
-
 joblib.dump(model, "models/student_model.pkl")
-print("Model saved.")
+print("Model trained and saved.")
